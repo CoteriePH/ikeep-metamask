6de4e79c2e1c66ad8f4613c00f19f8c6e47f6446
ericechemane <eechemane29@gmail.com>
2022-02-26 15:57:41 +0800

    added login with metamask

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,34 @@
-import useForm from '@hooks/useForm';
-import { BlueButton } from '@styled/Buttont';
-import Center from '@styled/Center';
-import Column from '@styled/Column';
-import Container from '@styled/Container';
-import Row from '@styled/Row';
-import Text from '@styled/Text';
-import TextInput from '@styled/TextInput';
+import { GetServerSideProps } from 'next';
+import cookie from 'cookie';
+
 import type { NextPage } from 'next';
 import React from 'react';
 
 const Home: NextPage = () => {
-
-  const { values, onInputHandler } = useForm({ username: 'das', password: 'asd' });
-
-  return <>
-    <Center direction='column' gap={1}>
-      <Text as='h1' m={1}> {values.username} {values.password} </Text>
-      <TextInput
-        name='username'
-        placeholder='Username'
-        onInput={onInputHandler} />
-      <TextInput
-        type='password'
-        name='password'
-        placeholder='Password'
-        onInput={onInputHandler} />
-      <BlueButton onClick={e => console.log('hello')}> Save </BlueButton>
-    </Center>
-  </>;
+  return <></>;
 };
-
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+
+  if (!context.req.headers.cookie) return {
+    redirect: { destination: '/index.html', permanent: false }
+  };
+  let ikeepSession = cookie.parse(context.req.headers.cookie).token;
+
+  const userHasSession =
+    !(ikeepSession === undefined ||
+      ikeepSession === '' ||
+      ikeepSession === null);
+
+  if (userHasSession)
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false
+      }
+    };
+  return {
+    redirect: { destination: '/index.html', permanent: false }
+  };
+};
---
+++ pages/login.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+import React from 'react';
+
+const Login: NextPage = () => {
+    return <>
+        <div> login </div>
+    </>;
+};
+export default Login;
